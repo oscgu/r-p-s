b8090717d9cb3195c6bef73123c7c7f1908b4436
Oscar Guertler <oscarguertler@gmail.com>
2022-07-14 23:53:01 +0200

    add logic

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
-
 contract Rps {
     enum Choices {
         ROCK,
         PAPER,
         SCISSORS
     }
+    Choices[3] winChoices = [Choices.SCISSORS, Choices.ROCK, Choices.PAPER];
 
     enum Winner {
         P1,
@@ -17,24 +15,60 @@ contract Rps {
         DRAW
     }
 
-    mapping(Choices => Choices) winCombination;
-
-    constructor() {
-        addWinCombination(Choices.PAPER, Choices.ROCK);
-        addWinCombination(Choices.ROCK, Choices.SCISSORS);
-        addWinCombination(Choices.SCISSORS, Choices.PAPER);
+    struct Wager {
+        uint256 tokenAmmount;
+        address tokenContract;
+        bytes32 p1EncryptedRPSChoice;
     }
 
-    function addWinCombination(Choices win, Choices lose) private {
-        winCombination[win] = lose;
+    struct Player {
+        address pAddress;
+        Wager[] wagers;
     }
-    
-    function play(Choices _p1, Choices _p2) public view returns(Winner winner) {
+
+    mapping(address => Player) players;
+
+    struct Round {
+        address p1;
+        address p2;
+        uint256 betAmmount;
+        Choices winChoice;
+    }
+    Round[] rounds;
+
+    function joinWager(address p2, address p1, uint256 wagerIndex, Choices p2Choice) public payable {
+        Wager[] memory pWager = players[p1].wagers;
+        require(pWager.length >= wagerIndex);
+
+        Wager memory wager = pWager[wagerIndex];
+        require(wager.tokenAmmount <= msg.value);
+    }
+
+    function revealChoice(address p1, uint256 wagerIndex, string memory movePw) public view returns (Choices) {
+        bytes32 encMove = sha256(abi.encodePacked(movePw));
+        require(encMove == players[p1].wagers[wagerIndex].p1EncryptedRPSChoice);
+
+        return Choices(uint8(bytes(movePw)[0]));
+    }
+
+    function mkWager(address p1Address, Wager calldata wager) public {
+        players[p1Address].wagers.push(wager);
+    }
+
+    function rmWager(address pAddress, uint8 wagerIndex) public {
+        Wager[] storage pWagers = players[pAddress].wagers;
+        require(pWagers.length != 0);
+        require(pWagers.length - 1 <= wagerIndex);
+
+        pWagers[wagerIndex] = pWagers[pWagers.length - 1];
+    }
+
+    function chooseWinner(Choices _p1, Choices _p2) public view returns(Winner winner) {
         if (_p1 == _p2) {
             return Winner.DRAW;
         }
 
-        if (winCombination[_p1] == _p2) {
+        if (winChoices[uint8(_p1)] == _p2) {
             return Winner.P1;
         }
         
