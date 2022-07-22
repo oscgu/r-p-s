3e72c273a465aed95c89351a8fe18b3a00ee2c69
Oscar Guertler <oscarguertler@gmail.com>
2022-07-22 22:41:48 +0200

    fix tests

--- test/RPS.ts
+++ test/RPS.ts
@@ -16,8 +16,6 @@ enum Result {
   DRAW
 }
 
-const TAX = 5;
-
 describe("Rock, Paper, Scissors", function () {
   async function deployRps() {
 
@@ -36,14 +34,14 @@ describe("Rock, Paper, Scissors", function () {
     const clearChoice = Choices.PAPER + "-" + "test";
     const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-    const tokenAmmount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+    const tokenamount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
 
-    await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+    await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
 
-    return { rps, p1, clearChoice, tokenAmmount };
+    return { rps, p1, clearChoice, tokenamount };
   };
 
-  describe("mkWager", function () {
+  describe("makeWager", function () {
     it("Should create a game", async function () {
       const { rps } = await loadFixture(deployRps);
       const [p1] = await ethers.getSigners();
@@ -51,13 +49,13 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = "2-test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const weiAmmount = BigNumber.from("100000000000000000"); /* 0.1 Eth */
+      const weiamount = BigNumber.from("100000000000000000"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: weiamount });
       const wager = await rps.connect(p1).getWager(p1.address, 0);
 
-      expect(wager.p1EncryptedRPSChoice).to.equal(hashedChoice);
-      expect(wager.tokenAmmount).to.equal(weiAmmount);
+      expect(wager.p1SaltedChoice).to.equal(hashedChoice);
+      expect(wager.tokenamount).to.equal(weiamount);
       expect(wager.hasP2).to.equal(false);
     });
 
@@ -68,20 +66,20 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const weiAmmount = BigNumber.from("900000000000000"); /* 0.09 Eth */
+      const weiamount = BigNumber.from("900000000000000"); /* 0.09 Eth */
 
-      await expect(rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount })).to.be.revertedWith("Bet ammount too low");
+      await expect(rps.connect(p1).makeWager(hashedChoice, { value: weiamount })).to.be.revertedWith("Bet amount too low");
     });
   });
 
   describe("joinWager", function () {
     it("Should let p2 join the game", async function () {
-      const { rps, p1, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, tokenamount } = await loadFixture(createGame);
       const [_, p2] = await ethers.getSigners();
       const p2Choice = Choices.PAPER;
       const wagerIndex = 0;
 
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
       const wager = await rps.connect(p1).getWager(p1.address, wagerIndex);
 
       expect(wager.hasP2).to.equal(true);
@@ -95,46 +93,46 @@ describe("Rock, Paper, Scissors", function () {
       const p2Choice = Choices.PAPER;
       const wagerIndex = 0;
 
-      await expect(rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: BigNumber.from("100000000") })).to.revertedWith("Tokenammount to low");
+      await expect(rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: BigNumber.from("100000000") })).to.revertedWith("Tokenamount to low");
     });
 
     it("Should revert on index out of bounds p2", async function () {
-      const { rps, p1, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, tokenamount } = await loadFixture(createGame);
       const [_, p2] = await ethers.getSigners();
       const p2Choice = Choices.PAPER;
       const wagerIndex = 1;
 
-      await expect(rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount })).to.revertedWith("Index out of bounds");
+      await expect(rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount })).to.revertedWith("Index out of bounds");
     });
 
     it("Should revert on player joining his own game", async function () {
-      const { rps, p1, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, tokenamount } = await loadFixture(createGame);
       const p1Choice = Choices.PAPER;
       const wagerIndex = 0;
 
-      await expect(rps.connect(p1).joinWager(p1.address, wagerIndex, p1Choice, { value: tokenAmmount })).to.revertedWith("You can't join your own game");
+      await expect(rps.connect(p1).joinWager(p1.address, wagerIndex, p1Choice, { value: tokenamount })).to.revertedWith("You can't join your own game");
     });
 
     it("Should revert if wager already has a second player", async function () {
-      const { rps, p1, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, tokenamount } = await loadFixture(createGame);
       const [_, p2, p3] = await ethers.getSigners();
       const p2Choice = Choices.PAPER;
       const p3Choice = Choices.ROCK;
       const wagerIndex = 0;
 
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
-      await expect(rps.connect(p3).joinWager(p1.address, wagerIndex, p3Choice, { value: tokenAmmount })).to.revertedWith("Wager already has a second player");
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
+      await expect(rps.connect(p3).joinWager(p1.address, wagerIndex, p3Choice, { value: tokenamount })).to.revertedWith("Wager already has a second player");
     });
   });
 
   describe("resolveWagerP1", function () {
     it("Should revert on index out of bounds", async function () {
-      const { rps, p1, clearChoice, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, clearChoice, tokenamount } = await loadFixture(createGame);
       const [_, p2] = await ethers.getSigners();
       const p2Choice = Choices.PAPER;
       const wagerIndex = 0;
 
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
 
       const oufOfBoundsIndex = 1;
 
@@ -148,11 +146,11 @@ describe("Rock, Paper, Scissors", function () {
     });
 
     it("Should let p1 resolve the wager", async function () {
-      const { rps, p1, clearChoice, tokenAmmount } = await loadFixture(createGame);
+      const { rps, p1, clearChoice, tokenamount } = await loadFixture(createGame);
       const [_, p2] = await ethers.getSigners();
       const wagerIndex = 0;
       const p2Choice = Choices.PAPER;
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
 
       await expect(await rps.connect(p1).resolveWagerP1(wagerIndex, clearChoice)).to.not.reverted;
     });
@@ -177,15 +175,15 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const tokenAmmount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+      const tokenamount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
 
       const [_, p2] = await ethers.getSigners();
       const wagerIndex = 0;
       const p2Choice = Choices.PAPER;
 
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
 
       await expect(rps.connect(p2).resolveWagerP2(p1.address, wagerIndex)).to.revertedWith("Timer didn't run out yet");
     });
@@ -199,9 +197,9 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const tokenAmmount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+      const tokenamount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
 
       const [_, p2] = await ethers.getSigners();
       const wagerIndex = 0;
@@ -218,13 +216,13 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const tokenAmmount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+      const tokenamount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
       const [_, p2] = await ethers.getSigners();
       const wagerIndex = 0;
       const p2Choice = Choices.PAPER;
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
       await rps.connect(p1).resolveWagerP1(wagerIndex, clearChoice);
 
       await expect(rps.connect(p1).getWager(p1.address, wagerIndex)).to.be.revertedWith("Index out of bounds");
@@ -239,16 +237,17 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const tokenAmmount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+      const tokenamount = ethers.utils.parseEther("0.1"); /* 0.1 Eth */
+      const revealTimeout = await (await rps.REVEAL_TIMEOUT());
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
       const [_, p2] = await ethers.getSigners();
       const wagerIndex = 0;
 
       const p2Choice = Choices.PAPER;
-      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenAmmount });
+      await rps.connect(p2).joinWager(p1.address, wagerIndex, p2Choice, { value: tokenamount });
 
-      await network.provider.send("evm_increaseTime", [172800]); //48 hours
+      await network.provider.send("evm_increaseTime", [revealTimeout]);
       await network.provider.send("evm_mine");
 
       const p2Bal = await p2.getBalance();
@@ -295,44 +294,66 @@ describe("Rock, Paper, Scissors", function () {
   });
 
   describe("chooseWinner", function () {
-    it("Should return p1 when paper and rock", async function () {
+    it("Should pay p1 when paper and rock", async function() {
       const { rps } = await loadFixture(deployRps);
-      const [p1] = await ethers.getSigners();
+      const [p1, p2] = await ethers.getSigners();
 
       const p1Choice = Choices.PAPER;
       const p2Choice = Choices.ROCK;
+      const bet = parseEther("0.1");
+      await rps.connect(p1).rcv({ value: bet.mul(2) });
 
-      await expect(await rps.connect(p1).chooseWinner(p1Choice, p2Choice)).to.equal(Result.P1);
+      const p1Bal = await p1.getBalance();
+      await rps.connect(p1).chooseWinner(p1Choice, p2Choice, p1.address, p2.address, bet);
+
+      await expect(await (await p1.getBalance()).sub(p1Bal)).to.approximately(bet.mul(2), parseEther("0.05"));
     });
 
     it("Should return p1 when rock and scissors", async function () {
       const { rps } = await loadFixture(deployRps);
-      const [p1] = await ethers.getSigners();
+      const [p1, p2] = await ethers.getSigners();
 
       const p1Choice = Choices.ROCK;
       const p2Choice = Choices.SCISSORS;
+      const bet = parseEther("0.1");
+      await rps.connect(p1).rcv({ value: bet.mul(2) });
 
-      await expect(await rps.connect(p1).chooseWinner(p1Choice, p2Choice)).to.equal(Result.P1);
+      const p1Bal = await p1.getBalance();
+      await rps.connect(p1).chooseWinner(p1Choice, p2Choice, p1.address, p2.address, bet);
+
+      await expect(await (await p1.getBalance()).sub(p1Bal)).to.approximately(bet.mul(2), parseEther("0.05"));
     });
 
     it("Should return p1 when scissors and paper", async function () {
       const { rps } = await loadFixture(deployRps);
-      const [p1] = await ethers.getSigners();
+      const [p1, p2] = await ethers.getSigners();
 
       const p1Choice = Choices.SCISSORS;
       const p2Choice = Choices.PAPER;
+      const bet = parseEther("0.1");
+      await rps.connect(p1).rcv({ value: bet.mul(2) });
 
-      await expect(await rps.connect(p1).chooseWinner(p1Choice, p2Choice)).to.equal(Result.P1);
+      const p1Bal = await p1.getBalance();
+      await rps.connect(p1).chooseWinner(p1Choice, p2Choice, p1.address, p2.address, bet);
+
+      await expect(await (await p1.getBalance()).sub(p1Bal)).to.approximately(bet.mul(2), parseEther("0.05"));
     });
 
     it("Should return draw when p1==p2", async function () {
       const { rps } = await loadFixture(deployRps);
-      const [p1] = await ethers.getSigners();
+      const [p1, p2] = await ethers.getSigners();
 
       const p1Choice = Choices.PAPER;
       const p2Choice = Choices.PAPER;
+      const bet = parseEther("0.1");
+      await rps.connect(p1).rcv({ value: bet.mul(2) });
 
-      await expect(await rps.connect(p1).chooseWinner(p1Choice, p2Choice)).to.equal(Result.DRAW);
+      const p1Bal = await p1.getBalance();
+      const p2Bal = await p2.getBalance();
+      await rps.connect(p1).chooseWinner(p1Choice, p2Choice, p1.address, p2.address, bet);
+
+      await expect(await (await p1.getBalance()).sub(p1Bal)).to.approximately(bet, parseEther("0.008"));
+      await expect(await (await p2.getBalance()).sub(p2Bal)).to.approximately(bet, parseEther("0.008"));
     });
   });
 
@@ -340,9 +361,9 @@ describe("Rock, Paper, Scissors", function () {
     it("Should revert if contract doesn't have enough tokens", async function () {
       const { rps } = await loadFixture(deployRps);
       const [p1] = await ethers.getSigners();
-      const tokenAmmount = utils.parseEther("1");
+      const tokenamount = utils.parseEther("1");
 
-      await expect(rps.payoutWithAppliedTax(p1.address, tokenAmmount)).to.revertedWith("Not enough tokens in contract");
+      await expect(rps.payoutWithAppliedTax(p1.address, tokenamount)).to.revertedWith("Not enough tokens in contract");
     })
 
     it("Should applay tax", async function () {
@@ -350,6 +371,7 @@ describe("Rock, Paper, Scissors", function () {
       const [p1] = await ethers.getSigners();
 
       const initialBet = ethers.utils.parseEther("0.5");
+      const TAX = await (await rps.TAX_PERCENT());
 
       const payout = (initialBet.mul(2)).sub(((initialBet.mul(2)).div(100)).mul(TAX));
       const expectedBal = (initialBet.mul(2)).sub(payout);
@@ -361,7 +383,7 @@ describe("Rock, Paper, Scissors", function () {
     })
   })
 
-  describe("rmWager", function() {
+  describe("removeWager", function() {
     it("Should remove a wager and update its position", async function() {
       const { rps } = await loadFixture(deployRps);
       const [p1] = await ethers.getSigners();
@@ -370,15 +392,15 @@ describe("Rock, Paper, Scissors", function () {
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
       const w1 = 0;
 
-      const weiAmmount = parseEther("0.1"); /* 0.1 Eth */
-      const weiAmmount2 = parseEther("0.2"); /* 0.1 Eth */
+      const weiamount = parseEther("0.1"); /* 0.1 Eth */
+      const weiamount2 = parseEther("0.2"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount });
-      await rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount2 });
+      await rps.connect(p1).makeWager(hashedChoice, { value: weiamount });
+      await rps.connect(p1).makeWager(hashedChoice, { value: weiamount2 });
 
-      await rps.connect(p1).rmWagerP1(p1.address, w1);
+      await rps.connect(p1).removeWagerP1(p1.address, w1);
       
-      expect(await (await rps.connect(p1).getWager(p1.address, w1)).tokenAmmount).to.equal(weiAmmount2);
+      expect(await (await rps.connect(p1).getWager(p1.address, w1)).tokenamount).to.equal(weiamount2);
     });
 
     it("Should forfeit if wager has p2", async function() {
@@ -389,13 +411,13 @@ describe("Rock, Paper, Scissors", function () {
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
       const w1 = 0;
 
-      const weiAmmount = parseEther("0.1"); /* 0.1 Eth */
+      const weiamount = parseEther("0.1"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount });
-      await rps.connect(p2).joinWager(p1.address, w1, Choices.PAPER, { value: weiAmmount});
+      await rps.connect(p1).makeWager(hashedChoice, { value: weiamount });
+      await rps.connect(p2).joinWager(p1.address, w1, Choices.PAPER, { value: weiamount});
       const p2Bal = await p2.getBalance();
 
-      await rps.connect(p1).rmWagerP1(p1.address, w1);
+      await rps.connect(p1).removeWagerP1(p1.address, w1);
       
       await expect((await p2.getBalance()).sub(p2Bal)).to.be.approximately(parseEther("0.19"), parseEther("0.01"));
     })
@@ -408,12 +430,12 @@ describe("Rock, Paper, Scissors", function () {
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
       const w1 = 0;
 
-      const weiAmmount = parseEther("0.1"); /* 0.1 Eth */
+      const weiamount = parseEther("0.1"); /* 0.1 Eth */
 
-      await rps.connect(p1).mkWager(hashedChoice, { value: weiAmmount });
-      await rps.connect(p2).joinWager(p1.address, w1, Choices.PAPER, { value: weiAmmount});
+      await rps.connect(p1).makeWager(hashedChoice, { value: weiamount });
+      await rps.connect(p2).joinWager(p1.address, w1, Choices.PAPER, { value: weiamount});
 
-      await expect(rps.connect(p2).rmWagerP1(p1.address, w1)).to.be.revertedWith("You can only remove your own wagers");
+      await expect(rps.connect(p2).removeWagerP1(p1.address, w1)).to.be.revertedWith("You can only remove your own wagers");
     })
   })
 
@@ -429,14 +451,14 @@ describe("Rock, Paper, Scissors", function () {
       const clearChoice = Choices.PAPER + "-" + "test";
       const hashedChoice = ethers.utils.soliditySha256(["string"], [clearChoice]);
 
-      const tokenAmmount = ethers.utils.parseEther("0.1");
+      const tokenamount = ethers.utils.parseEther("0.1");
 
       for (let i=0; i<100; i++) {
-        await rps.connect(p1).mkWager(hashedChoice, { value: tokenAmmount });
+        await rps.connect(p1).makeWager(hashedChoice, { value: tokenamount });
       }
 
       for (let i=0; i<100; i++) {
-        await rps.connect(p2).joinWager(p1.address, i, p2Choice, { value: tokenAmmount });
+        await rps.connect(p2).joinWager(p1.address, i, p2Choice, { value: tokenamount });
       }
 
       const p1Bal = await p1.getBalance();
