d12a3da64178d4e6371ce3c03cdbc4188e4f9332
Oscar Guertler <oscarguertler@gmail.com>
2022-07-22 22:41:43 +0200

    add func to change global vars, rm choosewinner

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.9;
 
 contract Rps {
     /* address public constant OWNER = ; */
-    uint public constant MIN_BET = 10000000 gwei; // 0.01 eth
-    uint8 public constant TAX_PERCENT = 5;
-    uint public constant REVEAL_TIMEOUT = 48 hours;
+    uint public MIN_BET = 10000000 gwei; // 0.01 eth
+    uint8 public TAX_PERCENT = 5;
+    uint32 public REVEAL_TIMEOUT = 48 hours;
+    address payable owner; /* 0x... */
 
     enum Choices {
         ROCK,
@@ -21,8 +22,8 @@ contract Rps {
     }
 
     struct Wager {
-        uint256 tokenAmmount;
-        bytes32 p1EncryptedRPSChoice;
+        uint256 tokenamount;
+        bytes32 p1SaltedChoice;
 
         bool hasP2;
         address p2;
@@ -37,12 +38,12 @@ contract Rps {
 
     mapping(address => Player) players;
 
-    function mkWager(bytes32 encChoice) public payable {
-        require(msg.value >= MIN_BET, "Bet ammount too low");
+    function makeWager(bytes32 encChoice) public payable {
+        require(msg.value >= MIN_BET, "Bet amount too low");
         Wager memory wager;
         wager.hasP2 = false;
-        wager.tokenAmmount = msg.value;
-        wager.p1EncryptedRPSChoice = encChoice;
+        wager.tokenamount = msg.value;
+        wager.p1SaltedChoice = encChoice;
 
         players[msg.sender].wagers.push(wager);
     }
@@ -55,7 +56,7 @@ contract Rps {
 
         Wager storage wager = wagers[wagerIndex];
         require(!wager.hasP2, "Wager already has a second player");
-        require(msg.value >= wager.tokenAmmount, "Tokenammount to low");
+        require(msg.value >= wager.tokenamount, "Tokenamount to low");
 
         wager.hasP2 = true;
         wager.p2 = msg.sender;
@@ -63,39 +64,42 @@ contract Rps {
         wager.timerStart = block.timestamp;
     }
 
-    function resolveWagerP1(uint256 wagerIndex, string memory movePw) public {
+    function resolveWagerP1(uint8 wagerIndex, string memory movePw) public {
         Wager memory wager = getWager(msg.sender, wagerIndex);
         require(wager.hasP2, "Wager doesn't have a second player");
 
-        Choices p1Choice = getHashChoice(wager.p1EncryptedRPSChoice, movePw);
-        Winner winner = chooseWinner(p1Choice, wager.p2Choice);
+        Choices p1Choice = getHashChoice(wager.p1SaltedChoice, movePw);
 
-        rmWager(msg.sender, wagerIndex);
-        rewardWinner(msg.sender, wager.p2, winner, wager.tokenAmmount);
+        removeWager(msg.sender, wagerIndex);
+        chooseWinner(p1Choice,wager.p2Choice, msg.sender, wager.p2, wager.tokenamount);
     }
 
-    function resolveWagerP2(address p1, uint256 wagerIndex) public {
+    function resolveWagerP2(address p1, uint8 wagerIndex) public {
         Wager memory wager = getWager(p1, wagerIndex);
         require(wager.hasP2, "Wager doesn't have a second player");
         require(didTimerRunOut(wager.timerStart), "Timer didn't run out yet");
 
-        rmWager(p1, wagerIndex);
-        payoutWithAppliedTax(msg.sender, wager.tokenAmmount);
+        removeWager(p1, wagerIndex);
+        payoutWithAppliedTax(msg.sender, wager.tokenamount);
     }
 
-    function rewardWinner(address p1, address p2, Winner winner, uint256 ammount) public {
-        if (winner == Winner.P1) {
-            payoutWithAppliedTax(p1, ammount);
-        } else if (winner == Winner.P2) {
-            payoutWithAppliedTax(p2, ammount);
-        } else if (winner == Winner.DRAW) {
-            payoutWithAppliedTax(p1, ammount / 2);
-            payoutWithAppliedTax(p2, ammount / 2);
+    function chooseWinner(Choices _p1, Choices _p2, address p1, address p2, uint256 amount) public {
+        if (_p1 == _p2) {
+            payoutWithAppliedTax(p1, amount / 2);
+            payoutWithAppliedTax(p2, amount / 2);
+            return;
         }
+
+        if (winChoices[uint8(_p1)] == _p2) {
+            payoutWithAppliedTax(p1, amount);
+            return;
+        }
+        
+        payoutWithAppliedTax(p2, amount);
     }
 
     /* private */
-    function rmWager(address p1,  uint256 wagerIndex) public {
+    function removeWager(address p1,  uint256 wagerIndex) public {
         Wager[] storage wagers = players[p1].wagers;
         require(wagers.length != 0, "No wagers to be removed");
         require(wagers.length >= wagerIndex + 1, "Index out of bounds");
@@ -105,7 +109,7 @@ contract Rps {
     }
 
     /* public */
-    function rmWagerP1(address p1, uint256 wagerIndex) public {
+    function removeWagerP1(address p1, uint8 wagerIndex) public {
         require(msg.sender == p1, "You can only remove your own wagers");
         Wager[] storage wagers = players[msg.sender].wagers;
         Wager memory wager = getWager(p1, wagerIndex);
@@ -114,27 +118,15 @@ contract Rps {
         wagers.pop();
 
         if (wager.hasP2) {
-            payoutWithAppliedTax(wager.p2, wager.tokenAmmount);
+            payoutWithAppliedTax(wager.p2, wager.tokenamount);
         }
-    }
-
-    function chooseWinner(Choices _p1, Choices _p2) public view returns(Winner winner) {
-        if (_p1 == _p2) {
-            return Winner.DRAW;
-        }
-
-        if (winChoices[uint8(_p1)] == _p2) {
-            return Winner.P1;
-        }
-        
-        return Winner.P2;
     }
 
     function payoutWithAppliedTax(address winner, uint256 initalBet) public {
-        uint256 ammount = (initalBet * 2) - (((initalBet * 2) / 100) * TAX_PERCENT);
-        require(address(this).balance > ammount, "Not enough tokens in contract");
+        uint256 amount = (initalBet * 2) - (((initalBet * 2) / 100) * TAX_PERCENT);
+        require(address(this).balance > amount, "Not enough tokens in contract");
 
-        payable(winner).transfer(ammount);
+        payable(winner).transfer(amount);
     }
 
     function getHashChoice(bytes32 hashChoice, string memory clearChoice) public pure returns (Choices) {
@@ -145,7 +137,7 @@ contract Rps {
     }
 
     /* Clear password = str */
-    function getChoiceFromStr(string memory str) public pure returns(Choices) {
+    function getChoiceFromStr(string memory str) public pure returns (Choices) {
         bytes1 first = bytes(str)[0];
 
         if (first == 0x30) {
@@ -161,11 +153,11 @@ contract Rps {
 
     function rcv() public payable {}
 
-    function didTimerRunOut(uint timerStart) private view returns (bool){
+    function didTimerRunOut(uint256 timerStart) private view returns (bool){
         return block.timestamp > timerStart + REVEAL_TIMEOUT;
     }
 
-    function getWager(address player, uint256 wagerIndex) public view returns(Wager memory) {
+    function getWager(address player, uint8 wagerIndex) public view returns (Wager memory) {
         Wager[] storage wagers = players[player].wagers;
         require(wagers.length >= wagerIndex + 1, "Index out of bounds");
 
@@ -176,19 +168,36 @@ contract Rps {
         return address(this).balance;
     }
 
-    function listWagers(address player) public view returns(Wager[] memory) {
+    function listWagers(address player) public view returns (Wager[] memory) {
         return players[player].wagers;
     }
 
-    function getTimeLeft(address player, uint wagerIndex) public view returns(uint) {
+    function getTimeLeft(address player, uint8 wagerIndex) public view returns (uint) {
         Wager memory wager = getWager(player, wagerIndex);
         require(!didTimerRunOut(wager.timerStart), "Timer already finished");
         require(wager.hasP2, "Timer didn't start yet");
         return REVEAL_TIMEOUT - (block.timestamp - wager.timerStart);
     }
 
-    function getWagerTokenammount(address player, uint wagerIndex) public view returns(uint) {
+    function getWagerTokenamount(address player, uint8 wagerIndex) public view returns (uint) {
         Wager memory wager = getWager(player, wagerIndex);
-        return wager.tokenAmmount;
+        return wager.tokenamount;
+    }
+
+    function ChangeMinBet(uint minBet) public onlyOwner {
+        MIN_BET = minBet;
+    }
+
+    function ChangeTaxPercent(uint8 taxPercent) public onlyOwner {
+        TAX_PERCENT = taxPercent;
+    }
+
+    function ChangeRevealTimeout(uint32 revealTimeout) public onlyOwner {
+        REVEAL_TIMEOUT = revealTimeout;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not the owner");
+        _;
     }
 }
